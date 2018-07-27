0431bd9bb6e35b7c5a1c27d5f550059e530ba225
Edison Higuita <edhiguitah@grupokonecta.com>
2018-07-27 16:56:19 -0500

    Se crea el repo generico y unit of worl

---
+++ DataModel/GenericRepository/GenericRepository.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.GenericRepository
+{
+    public class GenericRepository<TEntity> where TEntity: class
+    {
+
+        #region Private Members
+
+        internal Meucci3Entities Context;
+        internal DbSet<TEntity> DbSet;
+        #endregion  
+
+     
+        #region Constructor
+        public GenericRepository(Meucci3Entities context)
+        {
+            this.Context = context;
+            this.DbSet = context.Set<TEntity>();
+        }
+        #endregion  
+
+
+        public virtual IEnumerable<TEntity> Get()
+        {
+            IQueryable<TEntity> query = DbSet;
+
+            return query.ToList();
+        }
+
+
+        public virtual TEntity GetById(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = DbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Add(entityToDelete);
+            }
+
+            DbSet.Remove(entityToDelete);
+        }
+
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+
+        }
+
+        public IEnumerable<TEntity> GetMany(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).ToList();
+        }
+
+
+        public IQueryable<TEntity> GetManyQuerable(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).AsQueryable();
+        }
+
+
+        public TEntity Get(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).FirstOrDefault<TEntity>();
+        }
+
+
+        public void Delete(Func<TEntity, bool> where)
+        {
+            IQueryable<TEntity> objects = DbSet.Where<TEntity>(where).AsQueryable();
+
+            foreach (TEntity obj in objects)
+            {
+                DbSet.Remove(obj);
+            }
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return DbSet.ToList();
+        }
+
+
+        public IQueryable<TEntity> GetWithInclude(System.Linq.Expressions.Expression<Func<TEntity,bool>> predicate, 
+            params string[] include)
+        {
+            IQueryable<TEntity> query = this.DbSet;
+
+            query = include.Aggregate(query, (current, inc) => current.Include(inc));
+            return query.Where(predicate);
+        }
+
+        public bool Exists(object primaryKey)
+        {
+            return DbSet.Find(primaryKey) != null;
+        }
+
+        public TEntity GetSingle(Func<TEntity, bool> predicate)
+        {
+            return DbSet.Single<TEntity>(predicate);
+        }
+
+        public TEntity GetFirst(Func<TEntity, bool> predicate)
+        {
+            return DbSet.First<TEntity>();
+        }
+
+
+   
+
+
+    }
+}
---
+++ DataModel/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel.GenericRepository;
+using System.Data.Entity.Validation;
+using System.Diagnostics; 
+
+
+namespace DataModel.UnitOfWork
+{
+    public class UnitOfWork: IDisposable
+    {
+
+        private Meucci3Entities _context = null;
+        private GenericRepository<Cargos> _cargosRepository;
+
+
+        public UnitOfWork()
+        {
+            _context = new Meucci3Entities();
+        }
+
+
+        public GenericRepository<Cargos> CargosRepository 
+        {
+            get 
+            {
+                if (this._cargosRepository == null)
+                {
+                    this._cargosRepository = new GenericRepository<Cargos>(_context);
+                }
+
+                return _cargosRepository;
+
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+
+                var outputLines = new List<string>();
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    outputLines.Add(string.Format("{0}: Entity of type \"{1}\" in state \"{2}\" h " +
+                    "as the following validation errors:", DateTime.Now, eve.Entry.Entity.GetType().Name,
+                    eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        outputLines.Add(string.Format("- " +
+                        "Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+                    } 
+                }
+
+                System.IO.File.AppendAllLines(@"C:\errors.txt", outputLines);
+
+                throw e;
+            }
+        }
+
+
+        private bool disposed = false;
+
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    Debug.WriteLine("UnitOf Work is being disposed");
+                    _context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
